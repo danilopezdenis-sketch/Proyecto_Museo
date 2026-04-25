c8d7f6914e820307e26beaf1294a72a7d7107967
Dani <danilopezdenis@gmail.com>
2026-04-25 15:51:43 +0200

    actualizado

--- museo/next.config.mjs
+++ museo/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
-  serverExternalPackages: ['sequelize', 'mysql2', 'mongoose'],
+  serverExternalPackages: ['sequelize', 'mongoose'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('mysql2')
+    }
+    return config
+  },
 }
 
 export default nextConfig
