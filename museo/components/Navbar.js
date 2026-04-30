bb30409e794e809a83d6200dada9f783ad6b31ba
danilopezdenis-sketch <danilopezdenis@gmail.com>
2026-04-30 17:18:33 +0200

    Fix navbar brand link text

--- museo/components/Navbar.js
+++ museo/components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link href="/" className="navbar-brand">🏛 Museo</Link>
+      <Link href="/" className="navbar-brand">Museo</Link>
       <div className="navbar-links">
         <Link href="/obras">Obras</Link>
         <Link href="/artistas">Artistas</Link>
